c1f8fdbd60f0cc661ee20f4a0cbde510e0373cd5
Bruno Vollono <vollono.bruno@gmail.com>
2024-08-16 18:14:23 +0200

    continuato profilo.java

--- JBubbleBobble/src/Controller/Profilo.java
+++ JBubbleBobble/src/Controller/Profilo.java
@@ -3,9 +3,16 @@ import java.util.ArrayList;
 
 class Profilo {
     private String nickname; // nome utente, deciso nel costruttore
-    private Image avatar; // 
-    private ArrayList<Partita> partite;
-    private int livello;
+    private Image avatar; // immagine profilo, decisa nel costruttore
+    private ArrayList<Partita> partite; // storico partite
+
+    /*
+        Ogni profilo inizia dal livello 1.
+        Il livello viene incrementato tramite levelUp() quando il giocatore
+        supera determinate soglie di punti guadagnati durante le sue partite.
+        Tale controllo è effettuato durante addPartita().
+    */
+    private int livello; // livello del profilo, 
 
     public Profilo(String n, Image a){
         nickname = n;
@@ -35,5 +42,17 @@ class Profilo {
 
     public int getLivello(){
         return livello;
+    }
+
+    // incrementa di 1 il livello del profilo
+    public void levelUp(){
+        livello++;
+    }
+
+    // aggiunge una nuova partita allo storico del profilo + controlla la somma dei punteggi dell'utente
+    public void addPartita(Partita p){
+        partite.add(p);
+        
+        // TODO: controllo levelup, pensare a una funzione matematica
     }
 }
