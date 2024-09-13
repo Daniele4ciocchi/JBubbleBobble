1bdc30ca4be7291991d2941539c0c499a2e6ec7b
daniele4ciocchi <danielequattro45@gmail.com>
2024-09-13 18:24:03 +0200

    aggiornato Menu

--- JBubbleBobble/src/View/MenuView.java
+++ JBubbleBobble/src/View/MenuView.java
@@ -12,14 +12,34 @@ public class MenuView {
 
     public MenuView() {
         panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 1));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBackground(new Color(43,51,57));
+
+        ImageIcon titleIcon = new ImageIcon("src/sprites/Bubble_Bobble_Logo.png");
+        Image originalImage = titleIcon.getImage();
+        Image resizedImage = originalImage.getScaledInstance(150, 100, Image.SCALE_SMOOTH);
+        ImageIcon resizedIcon = new ImageIcon(resizedImage);
+        JLabel label = new JLabel(resizedIcon);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+
 
         nuovaPartitaButton = new JButton("Nuova Partita");
         continuaPartitaButton = new JButton("Continua Partita");
         visualizzaProfiloButton = new JButton("Visualizza Profilo");
 
+        nuovaPartitaButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        continuaPartitaButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        visualizzaProfiloButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        nuovaPartitaButton.setBackground(Color.GREEN);
+
+        panel.add(Box.createVerticalStrut(20));
+        panel.add(label);
+        panel.add(Box.createVerticalStrut(20));
         panel.add(nuovaPartitaButton);
+        panel.add(Box.createVerticalStrut(20));
         panel.add(continuaPartitaButton);
+        panel.add(Box.createVerticalStrut(20));
         panel.add(visualizzaProfiloButton);
     }
 
